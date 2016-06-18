52fc393985872d8ed8557c787159a2fbf36bcd14
Andrew Price <anwepr24@gmail.com>
2016-06-18 00:58:20 -0700

    Initial commit

---
+++ app/src/main/java/beam/lab001_icebreaker001/MainActivity.java
@@ -0,0 +1,15 @@
+package beam.lab001_icebreaker001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
