8316418ca634aa8e934f6323fefe72a4e8af4e8c
Spire55 <h>
2016-06-18 01:33:23 -0700

    Hi

--- app/src/main/java/beam/lab001_icebreaker001/MainActivity.java
+++ app/src/main/java/beam/lab001_icebreaker001/MainActivity.java
@@ -2,14 +2,34 @@ package beam.lab001_icebreaker001;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void breakIce()
+    {
+        count++;
+        if(count > 9)
+        {
+
+        }
+    }
+
+    private void levelWin()
+    {
+
+    }
+
 }
