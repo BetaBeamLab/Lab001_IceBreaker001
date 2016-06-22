dabc8a496c795f6cbc14c7f56899a145de25863c
Spire55 <h>
2016-06-21 18:35:25 -0700

    Hi

--- app/src/main/java/beam/lab001_icebreaker001/MainActivity.java
+++ app/src/main/java/beam/lab001_icebreaker001/MainActivity.java
@@ -3,12 +3,14 @@ package beam.lab001_icebreaker001;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity
 {
 
     private int count = 0;
+    boolean winState = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,19 +18,33 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button nextLevel = (Button) findViewById(R.id.button);
+
+
+        if(winState == false)
+        {
+            nextLevel.setVisibility(View.GONE);
+        }
+        else
+        {
+            nextLevel.setVisibility(View.VISIBLE);
+        }
+
+
     }
 
-    public void breakIce()
+    public void breakIce(View view)
     {
         count++;
         if(count > 9)
         {
-
+            levelWin();
         }
     }
 
-    private void levelWin()
+    public void levelWin()
     {
+        winState =  true;
 
     }
 
